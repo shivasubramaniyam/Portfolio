c40586ed2162449b8bef188abfe3b44fa218e951
shiva subramaniyam s <shivasubramaniyam54@gmail.com>
2025-08-09 15:40:13 +0530

    all files

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box } from "@chakra-ui/react"
+import Header from "@/components/ui/Header"
+
+export default function About(){
+    return(
+        <><Header />
+        <Box padding="100px" color={"red"}>
+            shiva
+        </Box></>
+    )
+}
---
+++ components/ui/Header.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Box, Heading,Menu,Flex} from "@chakra-ui/react";
+import Link from "next/link";
+
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+   <Heading>This is the heading
+    <Flex as="nav" gap={4}><Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+    <Link href="/project">Project</Link>
+    <Link href="/contact">Contact</Link></Flex>
+    </Heading>
+    </>
+  )
+}
