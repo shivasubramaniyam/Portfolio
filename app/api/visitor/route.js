d15d141e622df0325965a73de2d1a0c5d5664538
shiva subramaniyam s <shivasubramaniyam54@gmail.com>
2025-10-17 22:52:24 +0530

    build issue mod

--- app/api/visitor/route.js
+++ app/api/visitor/route.js
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import { MongoClient } from "mongodb";
-MONGODB_URI =
-  "mongodb+srv://shiva_2003:password@shivas-cluster.yqtctby.mongodb.net/?retryWrites=true&w=majority&appName=shivas-cluster";
 
-const uri = process.env.MONGO_URI || MONGODB_URI;
+const uri = process.env.MONGO_URI;
 
 if (!uri) {
   throw new Error(
